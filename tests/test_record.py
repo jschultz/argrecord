2fa17710b2c674e77b2018749475be2b1486c415
Jonathan Schultz <jonathan@schultz.la>
2020-01-13 13:25:10 +0800

    Removed debug line

--- tests/test_record.py
+++ tests/test_record.py
@@ -47,6 +47,4 @@ def test_copy():
     assert(os.path.isfile('out.txt'))
 
 if __name__ == '__main__':
-    print(sys.argv)
-    if len(sys.argv) > 1:
-        copy(sys.argv[1:])
+    copy(sys.argv[1:])
